da4d4feba0f0299df0e75c2fa31cd50e316f4624
fadhil-sanzoso <fadhil.ramadhan.s@gmail.com>
2026-02-12 10:41:48 +0700

    Add files via upload

---
+++ app_test.py
@@ -0,0 +1,12 @@
+
+import streamlit as st
+
+st.title("My First AI App 🚀")
+st.write("Hello! This is a simple Streamlit app.")
+
+name = st.text_input("What is your name?")
+if name:
+    st.write(f"Welcome to the world of AI, {name}!")
+
+
+    print("hello world")
